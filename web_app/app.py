a986a1e6e4128c3ea776f985276cf4ea9fdfa80c
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2025-11-26 22:20:10 +0700

    Add fitur passing isi form ke backend, logic error kalau tanggal ga diisi

--- web_app/app.py
+++ web_app/app.py
@@ -18,8 +18,43 @@ def detail_with_params(year, status):
     # print(year, status)
     return render_template("detail.html", curYear=year, curStat=status)
 
-@app.route("/predict/<satelit>")
+@app.route("/predict/<satelit>", methods=['GET', 'POST'])
 def predict(satelit):
+    # ambil tanggal dari form
+    if request.method == 'POST':
+        start_date = request.form.get("start_date")
+        end_date = request.form.get("end_date")
+        print(start_date)
+        print(end_date)
+
+        # cek tanggalnya diisi atau tidak
+        if start_date == "" or end_date == "":
+            error = "Tanggal mulai dan tanggal selesai harus diisi!"
+            print(error)
+
+            return render_template(
+                "predict.html",
+                satelit=satelit,
+                error=error,
+                start_date=start_date,
+                end_date=end_date,
+                show_segment=False
+            )
+        
+        # jalankan model machine learning sesuai tipe satelit yang dipilih
+        if satelit == 'sentinel':
+            print("do sentinel model")
+        else:
+            print("do landsat model")
+
+        return render_template(
+            "predict.html",
+            satelit=satelit,
+            start_date = start_date,
+            end_date = end_date,
+            show_img_container = "show"
+        )
+    
     return render_template('predict.html', satelit=satelit)
 
 @app.route("/comparison")
