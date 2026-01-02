b99a7646c5af916ca55bd488cd5fd0a3aeb96c19
TiffanyTasya <tiffany.tasyaagatha@gmail.com>
2026-01-02 15:16:04 +0700

    edit comment file coastline.py

--- modules/coastline.py
+++ modules/coastline.py
@@ -11,7 +11,20 @@ import random
 random.seed(42)
 np.random.seed(42)
 
-"""Melakukan proses sliding window untuk smoothing hasil prediksi"""
+"""
+Melakukan proses sliding window untuk smoothing hasil prediksi
+    Parameters
+    ----------
+    arr: numpy.ndarray
+        Array 2D biner (0 = darat, 1 = air).
+    window_size: int
+        Ukuran jendela (harus ganjil), default 7.
+
+    Returns
+    -------
+    result: numpy.ndarray
+        Array hasil setelah diproses sliding window.
+"""
 def sliding_window_majority(arr, window_size=7):
     pad = window_size // 2
     rows, cols = arr.shape
@@ -31,7 +44,22 @@ def sliding_window_majority(arr, window_size=7):
 
     return result
 
-"""Membersihkan noise kecil pada hasil prediksi dengan flood fill"""
+"""
+Membersihkan noise kecil pada hasil prediksi dengan flood fill
+    Parameters
+    ----------
+    mask: numpy.ndarray
+        Array 2D biner (0 = darat, 1 = air).
+    target_value: int 
+        Target kelas yang akan dibersihkan (0 = darat atau 1 = air)
+    min_size: int
+        Ukuran minimal pada 1 region.
+
+    Returns
+    -------
+    mask: numpy.ndarray
+        Array hasil setelah dibersihkan.
+"""
 def clean_mask(mask, target_value, min_size):
   # ambil kelas yang dipilih
   binary = (mask == target_value).astype(np.uint8)
@@ -46,7 +74,20 @@ def clean_mask(mask, target_value, min_size):
 
   return mask
 
-"""Membaca file GeoTIFF"""
+"""
+Membaca file GeoTIFF
+    Parameters:
+    -----------
+    filepath : str
+        Path ke file GeoTIFF
+
+    Returns:
+    --------
+    array: numpy.ndarray
+        Array 2D dengan nilai 0 (darat) dan 1 (air)
+    meta: dict
+        Metadata dari GeoTIFF (transform, crs, dll)
+"""
 def read_geotiff(filepath):
     with rasterio.open(filepath) as src:
         # Baca band pertama
@@ -64,7 +105,38 @@ def read_geotiff(filepath):
 
     return array, meta
 
-"""Ekstraksi garis pantai dari file GeoTIFF untuk Sentinel"""
+"""
+Ekstraksi garis pantai dari file GeoTIFF untuk Sentinel
+    Parameters:
+    -----------
+    filepath: str
+        Path ke file GeoTIFF (0=darat, 1=air)
+    year: str/int 
+        Keterangan tahun GeoTIFF
+    period: str
+        Keterangan periode GeoTIFF
+    water_value: int
+        Nilai yang merepresentasikan air dalam GeoTIFF (default 1)
+    land_value: int/float
+        Nilai yang merepresentasikan darat dalam GeoTIFF (default 0)
+    ws: int 
+        Nilai window size untuk proses sliding window (default 7)
+
+    Returns:
+    --------
+    ocean_mask: numpy.ndarray
+        Mask dari laut (tidak termasuk bercak air)
+    contours: list
+        List koordinat pixel untuk garis pantai
+    contours_geo: list
+        List koordinat geografis (lon, lat) untuk garis pantai
+    meta: dict
+        Metadata GeoTIFF
+    array: numpy.ndarray
+        Array 2D dengan nilai 0 (darat) dan 1 (air)
+    fig: figure
+        Gambar hasil garis pantai
+"""
 def extract_coastline_from_geotiff(filepath, year, period, water_value=1, land_value=0, ws = 7):
     array, meta = read_geotiff(filepath)
 
@@ -75,13 +147,7 @@ def extract_coastline_from_geotiff(filepath, year, period, water_value=1, land_v
     array = clean_mask(array, target_value=1, min_size=10000)
     array = clean_mask(array, target_value=0, min_size=500)
     array = sliding_window_majority(array, window_size=ws)
-
     water_mask = (array == water_value)
-    
-    # fig, ax = plt.subplots(figsize=(8, 8))
-    # ax.imshow(array, cmap="coolwarm")
-    # ax.axis("off")
-    # ax.set_title(f"Prediction After Smoothing - {year}_{period}")
     
     # tampilkan visualisasi hasil koreksi
     cmap = colors.ListedColormap(["#B40B27", "#3C4DC1"])
@@ -129,48 +195,23 @@ def extract_coastline_from_geotiff(filepath, year, period, water_value=1, land_v
 
     return ocean_mask, contours, contours_geo, meta, array, fig
 
-"""Ekstraksi garis pantai dari file GeoTIFF untuk Landsat"""
-def extract_coastline_from_geotiff_landsat(filepath, year, period, water_value=1, land_value=0, ws = 7):
-    raw, meta = read_geotiff(filepath)
-
-    if meta['nodata'] is not None:
-        raw = np.where(raw == meta['nodata'], land_value, raw)
-
-    # koreksi sliding window dan flood fill
-    array = clean_mask(raw, target_value=1, min_size=5000)
-    array = clean_mask(array, target_value=0, min_size=500)
-    array = sliding_window_majority(array, window_size=ws)
-    
-    # plot sebelum  smoothing 
-    # plt.figure(figsize=(8, 8))
-    # plt.imshow(raw, cmap="coolwarm")
-    # plt.axis('off')
-    # plt.title(f"Prediction Before Smoothing - {year}_{period}")
-    # plt.show()
-    
-    # plot setelah smoothing 
-    # plt.figure(figsize=(8, 8))
-    # plt.imshow(array, cmap="coolwarm")
-    # plt.axis('off')
-    # plt.title(f"Prediction After Smoothing - {year}_{period}")
-    # plt.show()
-
-    # deteksi kontur
-    contours = measure.find_contours(array.astype(float), 0.5)
-    transform = meta['transform']
-
-    # ubah koordinat piksel ke koordinat geografis (longitude, latitude)
-    contours_geo = []
-    for contour in contours:
-        geo_coords = []
-        for row, col in contour:
-            lon, lat = rasterio.transform.xy(transform, row, col)
-            geo_coords.append((lon, lat))
-        contours_geo.append(np.array(geo_coords))
-
-    return contours, contours_geo, meta, array
-
-"""Ekstraksi garis pantai dari input custom user"""
+"""
+Ekstraksi garis pantai dari input custom user
+    Parameters:
+    -----------
+    filepath: str
+        Path ke file GeoTIFF (0=darat, 1=air)
+    startDate: str 
+        Keterangan tanggal mulai data GeoTIFF
+    endDate: str
+        Keterangan tanggal akhir data GeoTIFF
+    water_value: int
+        Nilai yang merepresentasikan air dalam GeoTIFF (default 1)
+    land_value: int/float
+        Nilai yang merepresentasikan darat dalam GeoTIFF (default 0)
+    ws: int 
+        Nilai window size untuk proses sliding window (default 7)
+"""
 def extract_coastline_from_input(filepath, startDate, endDate, water_value=1, land_value=0, ws = 7):
     array, meta = read_geotiff(filepath)
     array = np.where(array == 58, 1, 0).astype(np.uint8)
